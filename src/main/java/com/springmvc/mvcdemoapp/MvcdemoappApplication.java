420e6bcec0a14a03a3421ab5b8f221e2b9c28230
Modith Prabath <mod9prabath@gmail.com>
2022-06-20 21:11:34 +0530

    Added Spring Boot Project

---
+++ src/main/java/com/springmvc/mvcdemoapp/MvcdemoappApplication.java
@@ -0,0 +1,13 @@
+package com.springmvc.mvcdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcdemoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcdemoappApplication.class, args);
+	}
+
+}
